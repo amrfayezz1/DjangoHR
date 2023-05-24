5a738b886a982e26d2942e0eb1ecd5267616bddb
malakwalid32 <malakwalid1222@icloud.com>
2023-05-25 00:03:06 +0200

    class employee/Vacations added,

--- project/pages/admin.py
+++ project/pages/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Vacations,Employee
+admin.site.register(Employee)
+admin.site.register(Vacations)
 
-# Register your models here.
--- project/pages/models.py
+++ project/pages/models.py
@@ -1,10 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class Employee(models.Model):
+    GENDER_CHOICES = [
+        ('male', 'Male'),
+        ('female', 'Female'),
+    ]
+
+    MARITAL_STATUS_CHOICES = [
+        ('single', 'Single'),
+        ('taken', 'Taken'),
+        ('married', 'Married'),
+        ('divorced', 'Divorced'),
+        ('widowed', 'Widowed'),
+    ]
+
+    ID = models.AutoField(primary_key=True)
+    Name = models.CharField(max_length=15)
+    Email = models.EmailField(max_length=254)
+    Address = models.CharField(max_length=100)
+    Phone = models.CharField(max_length=11)
+    Salary = models.CharField(max_length=20)
+    Date_of_birth = models.DateField()
+    Gender = models.CharField(max_length=6, choices=GENDER_CHOICES)
+    Marital_status = models.CharField(max_length=10, choices=MARITAL_STATUS_CHOICES)
+
 class Vacations(models.Model):
-    ID = models.ForeignKey(Employee , null = True , on_delete= models.CASCADE)
+    Employee_ID = models.ForeignKey(Employee, null=True, on_delete=models.CASCADE)
     Name = models.CharField(max_length=15)
     From = models.DateField()
     To = models.DateField()
-
     Reason = models.TextField()
